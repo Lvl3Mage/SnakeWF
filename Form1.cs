2b78672713edc520fb05c540ac6867983da011d5
Karl <lvlthreemage@gmail.com>
2023-05-04 20:32:28 +0200

    Made a rough working prototype

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Security.Cryptography;
+
 namespace SnakeWF
 {
     public enum Direction
@@ -12,15 +15,25 @@ namespace SnakeWF
 
         int width = 735;
         int height = 500;
-
+        Snake snake;
 
         public Form1() 
         {
             InitializeComponent();
-            Segment segment = new Segment(this, (50, 50), 100, 50, Direction.Down, Color.Blue);
+            snake = new Snake((100, 100), this);
+            GameLoop();
             // do later once the snake and segments have been written
         }
+        async void GameLoop()
+        {
+            while (true)
+            {
+                snake.Move(0.05f);
 
+                await Task.Delay(1);
+
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -28,6 +41,25 @@ namespace SnakeWF
 
         private void OnKeyDown(object sender, KeyEventArgs e)
         {
+            Direction newDir;
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    newDir = Direction.Up;
+                    break;
+                case Keys.S:
+                    newDir = Direction.Down;
+                    break;
+                case Keys.A:
+                    newDir = Direction.Left;
+                    break;
+                case Keys.D:
+                    newDir = Direction.Right;
+                    break;
+                default:
+                    return;
+            }
+            snake.RotateSnake(newDir);
             //Handle key presses and change the direction of the snake
         }
     }
@@ -39,13 +71,46 @@ namespace SnakeWF
             x = _x;
             y = _y;
         }
+        public IntVec((int,int) tup)
+        {
+            x = tup.Item1;
+            y = tup.Item2;
+        }
+        public (int,int) ToTuple()
+        {
+            return (x, y);
+        }
+
+
+        //operators
+
+        //basic addition
         public static IntVec operator -(IntVec other)
         {
             return new IntVec(-other.x, -other.y);
         }
         public static IntVec operator +(IntVec self, IntVec other)
         {
-            return new IntVec(self.x+other.x, other.y+other.y);
+            return new IntVec(self.x+other.x, self.y+other.y);
+        }
+        public static IntVec operator -(IntVec self, IntVec other)
+        {
+            return new IntVec(self.x - other.x, self.y - other.y);
+        }
+
+        //Integer scaling
+        public static IntVec operator *(IntVec self, int val)
+        {
+            return new IntVec(self.x*val, self.y*val);
+        }
+        public static IntVec operator *(int val, IntVec self)
+        {
+            return new IntVec(self.x * val, self.y * val);
+        }
+
+        public static IntVec operator /(IntVec self, int val)
+        {
+            return new IntVec(self.x / val, self.y / val);
         }
     }
     public class Snake 
@@ -53,82 +118,128 @@ namespace SnakeWF
         Segment head;
         List<Segment> segments = new List<Segment>();
         int pixelsPerSecond = 100;
-        int snakeWidth = 50;
+        int snakeWidth = 30;
+        Form attachedForm;
 
         //May need additional data such as:
             //The total snake length (in order to facilitate the snake update and point increment)
         
-        public Snake((int,int) position)
+        public Snake((int,int) position, Form form)
         {
+            attachedForm = form;
             //create the head
+            head = new Segment(attachedForm, position, snakeWidth, snakeWidth, Direction.Up, Color.Red);
 
+            IntVec newSegmentPos = new IntVec(head.position) - GetNormalVectorFromDir(head.direction) * snakeWidth;
+
+
+            //add new segment next to head with length 0
+            segments.Add(new Segment(attachedForm, newSegmentPos.ToTuple(), 1000, snakeWidth, head.direction, Color.Green));
             //create the first segment
+            //AddSegmentAtHead();
         }
         public void Move(float deltaTime)
         {
-
             //calculates the movement distance and movement vector
             int delta = (int)(pixelsPerSecond * deltaTime);
-            (int, int) movementVector = (0,0);
-            switch (head.direction)
-            {
-                case Direction.Up:
-                    movementVector = (0, delta);
-                    break;
-                case Direction.Down:
-                    movementVector = (0, -delta);
-                    break;
-                case Direction.Left:
-                    movementVector = (-delta, 0);
-                    break;
-                case Direction.Right:
-                    movementVector = (delta, 0);
-                    break;
-            }
             
-            //moves the head segment in its direction
-            (int, int) newHeadPos = head.position;
-            newHeadPos = (newHeadPos.Item1 + movementVector.Item1, newHeadPos.Item2 + movementVector.Item2);
-           
-            head.UpdateSegment(newHeadPos, snakeWidth, snakeWidth, head.direction);
+            IntVec movementVector = GetNormalVectorFromDir(head.direction)*delta;
 
+            //moves the head segment in its direction
+
+            IntVec newHeadPos = new IntVec(head.position) + movementVector;
+            //Debug.WriteLine($"pos: {head.position} new vec {movementVector.ToTuple()}, sum {(new IntVec(head.position) + movementVector).y} ");
+            head.UpdateSegment(newHeadPos.ToTuple(), snakeWidth, snakeWidth, head.direction);
+            //Debug.WriteLine(head.position);
 
             //updates the last segment to move to head with new length
             Segment closestSegment = segments[segments.Count-1];
 
-            (int, int) newClosestSegPos = closestSegment.position;
-            newClosestSegPos = (newClosestSegPos.Item1 + movementVector.Item1, newClosestSegPos.Item2 + movementVector.Item2);
+            IntVec newClosestSegPos = new IntVec(closestSegment.position) + movementVector;
 
-            closestSegment.UpdateSegment(newClosestSegPos, closestSegment.length + delta, snakeWidth, closestSegment.direction);
+            closestSegment.UpdateSegment(newClosestSegPos.ToTuple(), closestSegment.length + delta, snakeWidth, closestSegment.direction);
 
 
             //updates the first segment and removes it if length 0 and not last segment
+            int leftoverLength = delta;
 
-            Segment lastSegment = segments[0];
-            lastSegment.UpdateSegment(lastSegment.position, lastSegment.length - delta, snakeWidth, lastSegment.direction);
-            if(segments.Count > 1 && lastSegment.length <= 0) {
-                segments.RemoveAt(0);
+            
+
+            while (leftoverLength > 0) {
+                if (segments[0].length > leftoverLength)
+                {
+                    segments[0].UpdateSegment(segments[0].position, segments[0].length - leftoverLength, snakeWidth, segments[0].direction);
+                    break;
+                }
+                else
+                {
+                    int removedLength = segments[0].length;
+                    if(segments.Count > 1)
+                    {
+                        segments[0].RemoveSegment();
+                        segments.RemoveAt(0);
+                    }
+                    else
+                    {
+                        segments[0].UpdateSegment(segments[0].position, segments[0].length - leftoverLength, snakeWidth, segments[0].direction);
+                        break;
+                    }
+
+                    leftoverLength -= removedLength;
+                }
+
             }
         }
         public void RotateSnake(Direction direction)//rotates the snake, creating a new segment
         {
-            //rotatehead
-            //decrease the length of the last segment by the head movement
-            //add new segment next to head with length 0
+            //check for 180 degree rotation
+            IntVec combinedDir = GetNormalVectorFromDir(direction) + GetNormalVectorFromDir(head.direction);
+            if (combinedDir.x == 0 && combinedDir.y == 0)
+            {
+                return;
+            }
+            IntVec centerPos = new IntVec(head.position) - (GetNormalVectorFromDir(head.direction)*snakeWidth)/2;
+            //rotate head
+            head.UpdateSegment((centerPos + (GetNormalVectorFromDir(direction) * snakeWidth)/2).ToTuple(), head.length, snakeWidth, direction);
+            AddSegmentAtHead();
         }
-        (int, int) GetNormalVectorFromDir(Direction dir)
+
+        void AddSegmentAtHead()
+        {
+            IntVec newSegmentPos = new IntVec(head.position) - GetNormalVectorFromDir(head.direction) * snakeWidth;
+
+
+            //add new segment next to head with length 0
+            segments.Add(new Segment(attachedForm, newSegmentPos.ToTuple(), 0, snakeWidth, head.direction, Color.Green));
+        }
+        /*Direction GetOppositeDir(Direction dir)
         {
             switch (dir)
             {
                 case Direction.Up:
-                    return (0, 1);
+                    return Direction.Down;
                 case Direction.Down:
-                    return (0, -1);
+                    return Direction.Up;
                 case Direction.Left:
-                    return (0, -1);
+                    return Direction.Right;
                 case Direction.Right:
-                    return (0, 1);
-                default: return (0, 0);
+                    return Direction.Left;
+                default: return Direction.Up;
+            }
+        }*/
+        IntVec GetNormalVectorFromDir(Direction dir)//returns the direction represented as an IntVec
+        {
+            switch (dir)
+            {
+                case Direction.Up:
+                    return new IntVec(0, -1);
+                case Direction.Down:
+                    return new IntVec(0, 1);
+                case Direction.Left:
+                    return new IntVec(-1, 0);
+                case Direction.Right:
+                    return new IntVec(1, 0);
+                default: return new IntVec(0, 0);
             }
         }
             
@@ -147,7 +258,7 @@ namespace SnakeWF
         public int length { get; private set; }
         public (int, int) position { get; private set; }
         //Look into whether the intersections will be handled here or in the snake class
-        PictureBox visual { get; private set; }
+        public PictureBox visual { get; private set;}
         Color color;
         Form attachedForm;
         public Segment (Form form, (int, int) startPoint, int length, int width, Direction direction, Color color)
@@ -171,6 +282,8 @@ namespace SnakeWF
             int segmentX = startPoint.Item1;
             int segmentY = startPoint.Item2;
 
+
+            //adjusting picture box position depending on the direction and the width/length of the segment
             switch (direction)
             {
                 case Direction.Up:
@@ -201,9 +314,6 @@ namespace SnakeWF
                     break;
             }
         }
-        
-        // remove segment method that removes the segment from the form controls
-
         public void RemoveSegment ()
         {
             attachedForm.Controls.Remove(visual);
